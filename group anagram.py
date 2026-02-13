ffc746052d2600691e8b8d43d77539039d5ae80a
kaddambinee <yadavkaddambinee@gmail.com>
2026-02-13 10:26:48 +0530

    Add files via upload

---
+++ group anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))  # sorted word as key
+            groups[key].append(word)
+
+        return list(groups.values())
