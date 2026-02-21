8ae12b890d261360e1055885b97a679972d3d68e
kaddambinee <yadavkaddambinee@gmail.com>
2026-02-21 16:05:26 +0530

    Add files via upload

---
+++ 86.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        
+        before_head = ListNode(0)
+        after_head = ListNode(0)
+        
+        before = before_head
+        after = after_head
+        
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next
+        
+        after.next = None          # Important to avoid cycle
+        before.next = after_head.next
+        
+        return before_head.next
+        
